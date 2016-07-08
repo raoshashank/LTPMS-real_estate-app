69fbafeb8947afe4b20b10f708649d413933a233
Shashank Rao M <raoshashank73@gmail.com>
2016-07-08 14:21:52 +0530

    Finished Login UI

--- app/src/main/java/com/example/shashank/ltpms/MainActivity.java
+++ app/src/main/java/com/example/shashank/ltpms/MainActivity.java
@@ -1,15 +1,18 @@
 package com.example.shashank.ltpms;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
@@ -34,40 +37,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         final String URL_LOGIN = "http://192.168.0.101/webapp/login.php";
-        final EditText email_et = (EditText)findViewById(R.id.et1);
-        final EditText password_et = (EditText)findViewById(R.id.et2);
-        Button login = (Button)findViewById(R.id.login);
-  //      Button register = (Button)findViewById(R.id.register);
-          requestQueue = Volley.newRequestQueue(MainActivity.this);
+        final EditText email_et = (EditText) findViewById(R.id.email);
+        final EditText password_et = (EditText) findViewById(R.id.password);
+        TextView login = (TextView) findViewById(R.id.login);
+        TextView register = (TextView) findViewById(R.id.register);
+        register.setText(Html.fromHtml("<p><u>Sign Up</u></p>"));
+        //      Button register = (Button)findViewById(R.id.register);
+        requestQueue = Volley.newRequestQueue(MainActivity.this);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               StringRequest request = new StringRequest(Request.Method.POST, URL_LOGIN, new Response.Listener<String>() {
+                StringRequest request = new StringRequest(Request.Method.POST, URL_LOGIN, new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         try {
 
                             JSONObject jsonObject = new JSONObject(response);
-                            if(jsonObject.names().get(0).equals("success")){
-                                Toast.makeText(getApplicationContext(),jsonObject.names().getString(0).toString(),Toast.LENGTH_SHORT).show();
+                            if (jsonObject.names().get(0).equals("success")) {
+                                Toast.makeText(getApplicationContext(), jsonObject.names().getString(0).toString(), Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(getApplicationContext(), "fail", Toast.LENGTH_SHORT).show();
                             }
-                            else{
-                                Toast.makeText(getApplicationContext(),"fail",Toast.LENGTH_SHORT).show();
-                            }
 
 
+                        } catch (JSONException j) {
 
-                        }catch (JSONException j){
-
-                        };
-
-
-
-
+                        }
+                        ;
 
 
                     }
@@ -76,14 +74,14 @@ public class MainActivity extends AppCompatActivity {
                     public void onErrorResponse(VolleyError error) {
 
                     }
-                }){
+                }) {
 
                     @Override
                     protected Map<String, String> getParams() throws AuthFailureError {
-                        HashMap<String,String> hashMap = new HashMap<String, String>();
-                        hashMap.put("email",email_et.getText().toString());
+                        HashMap<String, String> hashMap = new HashMap<String, String>();
+                        hashMap.put("email", email_et.getText().toString());
 
-                        hashMap.put("password",password_et.getText().toString());
+                        hashMap.put("password", password_et.getText().toString());
                         return hashMap;
                     }
                 };
@@ -92,27 +90,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-    }
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
+            }
+        });
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
-        return super.onOptionsItemSelected(item);
+
     }
 }
